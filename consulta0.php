f9c7a7da942b9771ca36e698ba4112abc9726afa
Andrés Roldán <andresrold@gmail.com>
2015-09-17 11:43:48 +0200

    'Inicio'

---
+++ consulta0.php
@@ -0,0 +1,57 @@
+<?php
+$return = array();
+
+// NOMBRE DE LA APP
+$return['app_id'] = "1";
+$return['app_name'] = "Navidad 16";
+
+// MENÚ
+$return["menu"] = [
+	"num_items"			=> "1",
+	"menu_background"	=> "",
+	"menu_type"			=> "mosaic",
+	"items"				=> [
+		[
+			"section_id"	=> 1,
+			"item_name"		=> "Belenes",
+		],
+		[
+			"section_id"	=> 2,
+			"item_name"		=> "Dulces",
+		],
+	]
+];
+// SECCIONES
+$return["sections"] = [
+	[
+		"id"			=> 1,
+		"name" 			=> "Belenes",
+		"section_type"	=> "map",
+		"colors_palette" => [
+			"primary"				=> "",
+			"secondary"				=> "",
+			"tertiary"				=> "",
+			"quaternary"			=> "",
+			"background_primary" 	=> "",
+			"background_secondary"	=> ""
+		],
+	],
+	[
+		"id"			=> 2,
+		"name" 			=> "Dulces de Navidad",
+		"section_type"	=> "map",
+		"colors_palette" => [
+			"primary"				=> "#CC0066",
+			"secondary"				=> "#FFFFFF",
+			"tertiary"				=> "#202020",
+			"quaternary" 			=> "#AFAFAF",
+			"background_secondary"	=> "#F4F3EE",
+			"background_secondary"	=> "#F3F0EB"
+		],
+	],
+];
+
+
+header('Content-type: application/json');
+echo json_encode($return);
+?>
